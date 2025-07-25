18d42025c8564efec6dcf0597e44e397a0753152
Buniarena <bunhamin1234@gmail.com>
2025-07-25 20:17:28 +0200

    Update crypto_dashboard.py

--- crypto_dashboard.py
+++ crypto_dashboard.py
@@ -1,1 +1,6 @@
+import streamlit as st
 
+st.set_page_config(layout="wide")
+st.title("📊 Krypto Dashboard – BTC, PEPE, XVG")
+
+st.markdown("Ky është një dashboard për analizën e kriptovalutave nga Binance.")
